7fc4c2ab2eb0ab91a9d8f77befdd6315b66a5175
Valentin Haenel <vhaenel@anaconda.com>
2019-04-15 15:44:23 +0200

    overhaul and first running version, testing umap

--- switchboard.py
+++ switchboard.py
@@ -1,6 +1,7 @@
 import os
 import shlex
 import subprocess
+import json
 
 MINICONDA_BASE_URL = "https://repo.continuum.io/miniconda/"
 MINCONDA_FILE_TEMPLATE = "Miniconda3-latest-{}.sh"
@@ -8,6 +9,7 @@ MINCONDA_INSTALLER = "miniconda.sh"
 MINCONDA_PATH = "miniconda3"
 MINCONDA_FULL_PATH = os.path.join(os.getcwd(), MINCONDA_PATH)
 MINCONDA_BIN_PATH = os.path.join(MINCONDA_FULL_PATH, "bin")
+MINCONDA_CONDABIN_PATH = os.path.join(MINCONDA_FULL_PATH, "condabin")
 
 LINUX_X86 = "Linux-x86"
 LINUX_X86_64 = "Linux-x86_64"
@@ -15,6 +17,7 @@ MACOSX_X86_64 = "MacOSX-x86_64"
 
 
 def execute(command):
+    print(command)
     return subprocess.check_output(shlex.split(command))
 
 
@@ -37,13 +40,71 @@ def install_miniconda(install_path):
     execute("bash miniconda.sh -b -p {}".format(install_path))
 
 
-def inject_conda_path():
-    os.environ["PATH"] = MINCONDA_BIN_PATH + ":" + os.environ["PATH"]
+def inject_conda_path(miniconda_path):
+    os.environ["PATH"] = ":".join([MINCONDA_BIN_PATH, MINCONDA_CONDABIN_PATH] +
+                                  os.environ["PATH"].split(":"))
+
+
+def conda_switch_environment(env):
+    old = os.environ["PATH"].split(":")
+    env_bin_path = os.path.join(conda_environments()[env], "bin")
+    new = [env_bin_path] + old[1:]
+    os.environ["PATH"] = ":".join(new)
     print(os.environ["PATH"])
 
 
-def create_conda_env(name):
-    execute("conda create -n {}".format(name))
+def git_clone(url):
+    execute("git clone {}".format(url))
+
+
+def git_checkout(tag):
+    execute("git checkout {}".format(tag))
+
+
+def conda_update_conda():
+    execute("conda update -n base -c defaults conda")
+
+
+def conda_environments():
+    loaded_json = json.loads(execute("conda env list --json"))["envs"]
+    return dict(((os.path.basename(i), i) for i in loaded_json))
+
+
+def conda_create_env(name):
+    execute("conda create -y -n {}".format(name))
+
+
+def conda_install_numba_dev(env):
+    execute("conda install -y -n {} -c numba/label/dev numba".format(env))
+
+
+def conda_install(env, target):
+    execute("conda install -y -n {} {}".format(env, target))
+
+
+class UmapTests(object):
+
+    @property
+    def name(self):
+        return "umap"
+
+    @property
+    def clone_url(self):
+        return "https://github.com/lmcinnes/umap"
+
+    @property
+    def target_tag(self):
+        return "0.3.8"
+
+    @property
+    def conda_dependencies(self):
+        return ["numpy scikit-learn scipy nose"]
+
+    def install(self):
+        execute("pip install -e .")
+
+    def run_tests(self):
+        execute("nosetests -s umap")
 
 
 if __name__ == "__main__":
@@ -52,5 +113,18 @@ if __name__ == "__main__":
         wget_conda(url)
     if not os.path.exists(MINCONDA_FULL_PATH):
         install_miniconda(MINCONDA_FULL_PATH)
-    inject_conda_path()
-    create_conda_env("TEST")
+    inject_conda_path(MINCONDA_BIN_PATH)
+    conda_update_conda()
+    for project in [UmapTests()]:
+        if not os.path.exists(project.name):
+            git_clone(project.clone_url)
+            os.chdir(project.name)
+            git_checkout(project.target_tag)
+        if project.name not in conda_environments():
+            conda_create_env(project.name)
+            conda_switch_environment(project.name)
+            conda_install_numba_dev(project.name)
+            for dep in project.conda_dependencies:
+                conda_install(project.name, dep)
+        project.install()
+        project.run_tests()
