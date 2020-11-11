e4e5b1a0451e54668f8e4478ae6a495767876272
Valentin Haenel <vhaenel@anaconda.com>
2020-11-11 20:40:03 +0100

    try using --release not debug

--- switchboard.py
+++ switchboard.py
@@ -129,7 +129,7 @@ class AwkwardTests(GitTarget):
 
     @property
     def install_command(self):
-        return "git am ../0001-hack-cmake-to-use-j1.patch && pip install .[test,dev]"
+        return "git am ../0001-hack-cmake-to-use-j1.patch && python localbuild.py -j1 --release"
 
     # install need to futz about with the environment
     def install(self):
