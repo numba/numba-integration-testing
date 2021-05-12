46cab0e70006628716c5cbdefa932bfb0d8bf51e
KevinCawley <cawleyke@msu.edu>
2021-05-12 18:40:33 -0500

    removed some dependency

--- switchboard.py
+++ switchboard.py
@@ -327,9 +327,9 @@ class TardisTests(GitTarget):
     def conda_dependencies(self):
         return ["-c conda-forge python=3 pip numpy=1.19 scipy=1.5 "
     "pandas=1.0 astropy=3 numexpr networkx pyyaml jsonschema "
-    "pyne=0.7 pytables h5py requests tqdm beautifulsoup4 lxml "
-    "jupyter notebook matplotlib graphviz pygraphviz ipywidgets "
-    "qgrid plotly pytest=5 requests parso=0.8 pickle5"]
+    "pyne=0.7 pytables h5py requests tqdm "
+    "matplotlib graphviz pygraphviz ipywidgets "
+    "qgrid plotly pytest=5 requests parso=0.8"]
     
     @property
     def install_command(self):
